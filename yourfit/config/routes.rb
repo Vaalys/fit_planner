9e98195c7ec0fb279a9e967c985a6d78c16d982b
Ammie Mcculloch <vaalys9@gmail.com>
2015-06-03 19:34:39 -0400

    more views, and routes set up for profiles

--- yourfit/config/routes.rb
+++ yourfit/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
 
+  resources :users do
+    resources :profile
+  end
+  
   get '/', to: 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
